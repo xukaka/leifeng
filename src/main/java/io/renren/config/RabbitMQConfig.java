8a9d9fe69b71f8fab2b466ab95f1369db3821af3
jiangzhou_liu <jiangzhou_liu@kingdee.com>
2019-04-10 10:10:10 +0800

    jenkins编译报错处理2

--- src/main/java/io/renren/config/RabbitMQConfig.java
+++ src/main/java/io/renren/config/RabbitMQConfig.java
@@ -1,8 +1,8 @@
 package io.renren.config;
 
-import org.springframework.amqp.core.Queue;
+/*import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;*/
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
--- src/main/java/io/renren/modules/app/event/RenrenMsgListener.java
+++ src/main/java/io/renren/modules/app/event/RenrenMsgListener.java
@@ -1,19 +1,15 @@
 package io.renren.modules.app.event;
 
-import com.alibaba.fastjson.JSONObject;
-import io.renren.config.RabbitMQConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.amqp.rabbit.annotation.RabbitHandler;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
-@RabbitListener(queues = RabbitMQConfig.QUEUE_NAME)
+//@RabbitListener(queues = RabbitMQConfig.QUEUE_NAME)
 public class RenrenMsgListener {
     private static Logger logger = LoggerFactory.getLogger(RenrenMsgListener.class);
 
-    @RabbitHandler
+    //    @RabbitHandler
     public void handleMessage(String message) {
         logger.info(message);
 //        JSONObject jsonObject = JSONObject.parseObject(message);
