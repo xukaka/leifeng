1385a5db163a71dd0286105e9133d2108a533540
jiangzhou <1139963253@qq.com>
2019-04-15 23:40:08 +0800

    领取任务接口调试3

--- src/main/java/io/renren/modules/app/dao/task/TaskReceiveDao.java
+++ src/main/java/io/renren/modules/app/dao/task/TaskReceiveDao.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.mapper.BaseMapper;
 import io.renren.modules.app.entity.setting.Member;
 import io.renren.modules.app.entity.task.TaskReceiveEntity;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 /**
  * 任务领取
@@ -16,5 +17,5 @@ public interface TaskReceiveDao extends BaseMapper<TaskReceiveEntity> {
      * 获取任务领取人
      * @return
      */
-    Member getReceiver(Long receiveId);
+    Member getReceiver(@Param("receiveId") Long receiveId);
 }
