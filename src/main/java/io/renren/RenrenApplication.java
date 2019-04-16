780a9104104d938d850df70a3d508943114e34c0
809508059@qq.com <809508059@qq.com>
2019-04-17 02:33:40 +0800

    更新IO组件，引入J-im，底层为T-io。 修改文件：pom，由于2.6版本的t-im需要打包本地仓库，所以暂时使用2.5 修改文件：logback-spring.xml为loback.xml并增加j-im日志 以及相关测试代码。

--- src/main/java/io/renren/RenrenApplication.java
+++ src/main/java/io/renren/RenrenApplication.java
@@ -1,6 +1,15 @@
 package io.renren;
 
+import io.renren.common.io.command.IoWsHandshakeProcessor;
+import io.renren.common.io.listener.ImDemoGroupListener;
+import io.renren.common.io.service.LoginServiceProcessor;
 import io.renren.datasources.DynamicDataSourceConfig;
+import org.jim.common.ImConfig;
+import org.jim.common.packets.Command;
+import org.jim.server.ImServerStarter;
+import org.jim.server.command.CommandManager;
+import org.jim.server.command.handler.HandshakeReqHandler;
+import org.jim.server.command.handler.LoginReqHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -15,11 +24,33 @@ import org.springframework.context.annotation.Import;
 public class RenrenApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
+		ImConfig imConfig = new ImConfig();
+		imConfig.setBindPort(8080);
+		//初始化SSL;(开启SSL之前,你要保证你有SSL证书哦...)
+		//设置群组监听器，非必须，根据需要自己选择性实现;
+		imConfig.setImGroupListener(new ImDemoGroupListener());
+		ImServerStarter imServerStarter = new ImServerStarter(imConfig);
+		/*****************start 以下处理器根据业务需要自行添加与扩展，每个Command都可以添加扩展,此处为demo中处理**********************************/
+		HandshakeReqHandler handshakeReqHandler = CommandManager.getCommand(Command.COMMAND_HANDSHAKE_REQ, HandshakeReqHandler.class);
+		//添加自定义握手处理器;
+		handshakeReqHandler.addProcessor(new IoWsHandshakeProcessor());
+		LoginReqHandler loginReqHandler = CommandManager.getCommand(Command.COMMAND_LOGIN_REQ,LoginReqHandler.class);
+		//添加登录业务处理器;
+		loginReqHandler.addProcessor(new LoginServiceProcessor());
+		/*****************end *******************************************************************************************/
+		try {
+			imServerStarter.start();
+
+		}catch (Exception e){
+
+		}
 		SpringApplication.run(RenrenApplication.class, args);
 	}
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+
 		return application.sources(RenrenApplication.class);
 	}
+
 }
---
+++ src/main/java/io/renren/common/io/listener/ImDemoGroupListener.java
@@ -0,0 +1,38 @@
+package io.renren.common.io.listener;
+
+import org.jim.common.ImAio;
+import org.jim.common.ImPacket;
+import org.jim.common.ImSessionContext;
+import org.jim.common.packets.*;
+import org.jim.server.listener.ImGroupListener;
+import org.tio.core.ChannelContext;
+
+
+public class ImDemoGroupListener extends ImGroupListener {
+	/** 
+	 * @param channelContext
+	 * @param group
+	 * @throws Exception
+	 * @author: WChao
+	 */
+	@Override
+	public void onAfterUnbind(ChannelContext channelContext, String group) throws Exception {
+		//发退出房间通知  COMMAND_EXIT_GROUP_NOTIFY_RESP
+		ImSessionContext imSessionContext = (ImSessionContext)channelContext.getAttribute();
+		ExitGroupNotifyRespBody exitGroupNotifyRespBody = new ExitGroupNotifyRespBody();
+		exitGroupNotifyRespBody.setGroup(group);
+		Client client = imSessionContext.getClient();
+		if(client == null)
+			return;
+		User clientUser = client.getUser();
+		if(clientUser == null)
+			return;
+		User notifyUser = new User(clientUser.getId(),clientUser.getNick());
+		exitGroupNotifyRespBody.setUser(notifyUser);
+		
+		RespBody respBody = new RespBody(Command.COMMAND_EXIT_GROUP_NOTIFY_RESP,exitGroupNotifyRespBody);
+		ImPacket imPacket = new ImPacket(Command.COMMAND_EXIT_GROUP_NOTIFY_RESP, respBody.toByte());
+		ImAio.sendToGroup(group, imPacket);
+		
+	}
+}
