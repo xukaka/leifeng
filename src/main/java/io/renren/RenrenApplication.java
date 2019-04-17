9597a60196e188f40f83ca57e865b855571f846d
809508059@qq.com <809508059@qq.com>
2019-04-17 21:45:31 +0800

    切换socket为ws模式测试

--- src/main/java/io/renren/RenrenApplication.java
+++ src/main/java/io/renren/RenrenApplication.java
@@ -34,7 +34,7 @@ public class RenrenApplication extends SpringBootServletInitializer {
 	private static Logger logger = LoggerFactory.getLogger(RenrenApplication.class);
 	public static void main(String[] args) {
 		ImConfig imConfig = new ImConfig();
-		imConfig.setBindIp("47.94.164.57");
+		imConfig.setBindIp("127.0.0.1");
 		imConfig.setBindPort(11805);
 //		imConfig.setIsSSL("on");
 //		try {
