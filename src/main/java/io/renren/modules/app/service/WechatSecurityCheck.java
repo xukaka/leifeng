7ace5cec1536d955d5bb2dc6792cc592520ae338
jiangzhou <1139963253@qq.com>
2019-07-21 20:14:35 +0800

    微信关键字安全检查

--- src/main/java/io/renren/modules/app/service/WechatSecurityCheck.java
+++ src/main/java/io/renren/modules/app/service/WechatSecurityCheck.java
@@ -13,12 +13,14 @@ import org.apache.http.entity.StringEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+@Component
 public class WechatSecurityCheck {
     private final static Logger logger = LoggerFactory.getLogger(WechatSecurityCheck.class);
 
