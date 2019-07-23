693fbbc56ebc2b1ccfaebf9088e18a80477fb145
jiangzhou_liu <jiangzhou_liu@kingdee.com>
2019-07-23 18:18:15 +0800

    redis缓存banner

--- src/main/java/io/renren/common/utils/RedisKeys.java
+++ src/main/java/io/renren/common/utils/RedisKeys.java
@@ -6,6 +6,7 @@ package io.renren.common.utils;
 public class RedisKeys {
 
     public static String BANNER_KEY = "redis:task:banner:key:";
+    public static String BANNER_TYPE_KEY = "redis:task:banner:type:key:";
     public static String PHONE_CODE_KEY = "redis:validate:phonecode:key:";//短信验证
     public static String HOT_SEARCH = "redis:hot:search:key:";//热门搜索
     public static String CHECK_IN = "redis:checkin:key:";//签到
--- src/main/java/io/renren/modules/app/service/impl/BannertImpl.java
+++ src/main/java/io/renren/modules/app/service/impl/BannertImpl.java
@@ -1,11 +1,16 @@
 package io.renren.modules.app.service.impl;
 
 import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import io.renren.common.utils.DateUtils;
 import io.renren.common.utils.PageUtils;
+import io.renren.common.utils.RedisKeys;
+import io.renren.common.utils.RedisUtils;
 import io.renren.common.validator.ValidatorUtils;
 import io.renren.modules.app.dao.banner.BannerDao;
 import io.renren.modules.app.dto.BannerDto;
+import io.renren.modules.app.dto.TaskBannerDto;
 import io.renren.modules.app.entity.BannerTypeEnum;
 import io.renren.modules.app.entity.banner.BannerEntity;
 import io.renren.modules.app.form.BannerForm;
@@ -13,24 +18,44 @@ import io.renren.modules.app.form.BannerUpdateForm;
 import io.renren.modules.app.form.PageWrapper;
 import io.renren.modules.app.service.BannerService;
 import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.GsonJsonParser;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 
+import javax.annotation.Resource;
+import java.lang.reflect.Type;
 import java.util.List;
 
 
 @Service
 public class BannertImpl extends ServiceImpl<BannerDao, BannerEntity> implements BannerService {
 
+    private static final int BANNERS_EXPIRE = 2 * 60 * 60;//2小时;
+
+    private static final Type gsonType = new TypeToken<PageUtils<BannerDto>>() {
+    }.getType();
+
+    @Resource
+    private RedisUtils redisUtils;
 
     @Override
     public PageUtils<BannerDto> getBanners(BannerTypeEnum type, PageWrapper page) {
-        List<BannerDto> banners = baseMapper.getBanners(type, page);
-        if (CollectionUtils.isEmpty(banners)) {
-            return new PageUtils<>();
+        String redisKey = RedisKeys.BANNER_TYPE_KEY + type.name();
+        String json = redisUtils.get(redisKey);
+        if (!StringUtils.isEmpty(json)) {
+            return new Gson().fromJson(json, gsonType);
+        } else {
+            List<BannerDto> banners = baseMapper.getBanners(type, page);
+            if (CollectionUtils.isEmpty(banners)) {
+                return new PageUtils<>();
+            }
+            int total = baseMapper.count(type);
+            PageUtils<BannerDto> result = new PageUtils<>(banners, total, page.getPageSize(), page.getCurrPage());
+            redisUtils.set(redisKey, result, BANNERS_EXPIRE);
+            return result;
         }
-        int total = baseMapper.count(type);
-        return new PageUtils<>(banners, total, page.getPageSize(), page.getCurrPage());
     }
 
     @Override
